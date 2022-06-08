2a31e858ff0367616051893cb27f47f13db1b2ec
iceiscold <90751182+alirafee21@users.noreply.github.com>
2022-06-08 01:20:57 -0400

    server and client bug fix

--- socket_client.py
+++ socket_client.py
@@ -1,43 +1,51 @@
-from http import client
 import socket
-import select
 import errno
 import sys
 from threading import Thread
+
 HEADER_LENGTH = 10
+client_socket = None
 
-
-def connect(IP, PORT, my_username, error_callback):
+# Connects to the server
+def connect(ip, port, my_username, error_callback):
 
     global client_socket
+
+    # Create a socket
+    # socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+    # socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     try:
-        client_socket.connect((IP, PORT))
+        # Connect to a given ip and port
+        client_socket.connect((ip, port))
     except Exception as e:
         # Connection error
         error_callback('Connection error: {}'.format(str(e)))
         return False
-# client_socket.setblocking(False)
 
+    # Prepare username and header and send them
+    # We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
     username = my_username.encode('utf-8')
     username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
     client_socket.send(username_header + username)
+
     return True
 
+# Sends a message to the server
 def send(message):
+    # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
     message = message.encode('utf-8')
     message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
     client_socket.send(message_header + message)
 
-
+# Starts listening function in a thread
+# incoming_message_callback - callback to be called when new message arrives
+# error_callback - callback to be called on error
 def start_listening(incoming_message_callback, error_callback):
     Thread(target=listen, args=(incoming_message_callback, error_callback), daemon=True).start()
 
-
-
-
-
-
+# Listens for incomming messages
 def listen(incoming_message_callback, error_callback):
     while True:
 
@@ -59,14 +67,14 @@ def listen(incoming_message_callback, error_callback):
                 username = client_socket.recv(username_length).decode('utf-8')
 
                 # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
-                # message_header = client_socket.recv(HEADER_LENGTH)
-                message_length = int(username_header.decode('utf-8').strip())
+                message_header = client_socket.recv(HEADER_LENGTH)
+                message_length = int(message_header.decode('utf-8').strip())
                 message = client_socket.recv(message_length).decode('utf-8')
 
                 # Print message
                 incoming_message_callback(username, message)
 
-
+        
         except IOError as e:
        
             if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
@@ -74,7 +82,6 @@ def listen(incoming_message_callback, error_callback):
                 sys.exit()
 
             continue
-
         except Exception as e:
             # Any other exception - something happened, exit
             error_callback('Reading error: {}'.format(str(e)))
